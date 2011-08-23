8b43598f6cfe786984615170aa98851fa93780f7
Greg Shackles <gshackles@gmail.com>
2011-08-22 23:47:59 -0400

    style updates, page nav transitions

--- NycCodeCamp.WP7App/App.xaml.cs
+++ NycCodeCamp.WP7App/App.xaml.cs
@@ -102,7 +102,7 @@ namespace NycCodeCamp.WP7App
 
             // Create the frame but don't set it as RootVisual yet; this allows the splash
             // screen to remain active until the application is ready to render.
-            RootFrame = new PhoneApplicationFrame();
+            RootFrame = new TransitionFrame();
             RootFrame.Navigated += CompleteInitializePhoneApplication;
 
             // Handle navigation failures
