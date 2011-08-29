45be8dd11048e29c91244e19af42be6fba28cc69
Greg Shackles <gshackles@gmail.com>
2011-08-28 22:15:44 -0400

    add fake session support to wp7

--- NycCodeCamp.WP7App/ValueConverters/StringNullOrEmptyToVisibilityValueConverter.cs
+++ NycCodeCamp.WP7App/ValueConverters/StringNullOrEmptyToVisibilityValueConverter.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace NycCodeCamp.WP7App.ValueConverters
 {
-    public class StringNullOrEmptyToBooleanValueConverter : IValueConverter
+    public class StringNullOrEmptyToVisibilityValueConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return string.IsNullOrEmpty(value.ToString());
+            return string.IsNullOrEmpty(value.ToString())
+                    ? Visibility.Collapsed
+                    : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
